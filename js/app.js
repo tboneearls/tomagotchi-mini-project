df14b59492b4592f7610c796f21a4ffa06b35903
Tyler Earls <tyler.a.earls@gmail.com>
2018-03-27 14:17:38 -0500

    created tomagotchi class

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log("javascript is working");
+
+class Tomagotchi {
+	constructor (name, age, hunger, sleepiness, boredom) {
+		this.name = name;
+		this.age = age;
+		this.hunger = hunger;
+		this.sleepiness = sleepiness;
+		this.boredom = boredom;
+	}
+}
+
+const displayPet = () =>{
+	// image to display tomagotchi
+}
