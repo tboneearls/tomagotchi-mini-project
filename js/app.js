5e5552ee566eccb52e83183968f5dde86aeed7fa
Tyler Earls <tyler.a.earls@gmail.com>
2018-03-27 14:32:12 -0500

    character for tomagotchi displayed in browser

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
 console.log("javascript is working");
 
 class Tomagotchi {
-	constructor (name, age, hunger, sleepiness, boredom) {
+	constructor (name, age) {
 		this.name = name;
 		this.age = age;
-		this.hunger = hunger;
-		this.sleepiness = sleepiness;
-		this.boredom = boredom;
+		// hunger, sleepiness, boredom should start at 0,
+		// then increase incrementally
+		this.hunger = 0;
+		this.sleepiness = 0;
+		this.boredom = 0;
 	}
 }
 
 const displayPet = () =>{
 	// image to display tomagotchi
+}
+const raiseHunger = () => {
+	// raise hunger by set interval
+}
+const raiseBoredom = () => {
+	// raise boredom by set interval
+}
+const raiseSleepiness = () => {
+	// raise sleepiness by set interval
+}
+const raiseAge = ()=> {
+	// raise age in increments of x minutes
 }
